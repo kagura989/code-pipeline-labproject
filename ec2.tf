a1ce62fa62f70358b5b830845e74433d9580d6ea
kagura989 <k36955692@gmail.com>
2025-07-20 21:31:04 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,100 @@
+# IAM Role for EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_policy_attachment" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2RoleforAWSCodeDeploy"
+}
+
+# Instance Profile (IAM role attachment)
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-codedeploy-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+# Security Group for EC2
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-codedeploy-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "codedeploy_instance" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  subnet_id                   = data.aws_subnet.default.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
+  key_name                    = var.key_name
+
+  tags = {
+    Name = "code-pipeline"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y ruby wget
+              cd /home/ec2-user
+              wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+              chmod +x ./install
+              ./install auto
+              systemctl start codedeploy-agent
+              systemctl enable codedeploy-agent
+              EOF
+}
+
+# Fetch a default subnet
+data "aws_subnet" "default" {
+  default_for_az = true
+  availability_zone = "ap-south-1a"
+}
