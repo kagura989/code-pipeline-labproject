159f376cc43125893af437dd9e021f1fb41a14aa
kagura989 <k36955692@gmail.com>
2025-07-20 21:20:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,130 @@
+# CodeDeploy IAM Role
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codedeploy_policy" {
+  role = aws_iam_role.codedeploy_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:*",
+          "autoscaling:*",
+          "elasticloadbalancing:*",
+          "cloudwatch:*",
+          "s3:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# CodeDeploy Application
+resource "aws_codedeploy_app" "app" {
+  name = var.codedeploy_app_name
+  compute_platform = "Server"
+}
+
+# CodeDeploy Deployment Group
+resource "aws_codedeploy_deployment_group" "deployment_group" {
+  app_name              = aws_codedeploy_app.app.name
+  deployment_group_name = var.codedeploy_group_name
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
+
+  ec2_tag_filter {
+    key   = var.ec2_tag_filter["Name"]
+    type  = "KEY_AND_VALUE"
+    value = var.ec2_tag_filter["Value"]
+  }
+
+  deployment_style {
+    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
+    deployment_type   = "IN_PLACE"
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+}
+
+# CodePipeline
+resource "aws_codepipeline" "pipeline" {
+  name     = "codepipeline-lab"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.artifacts.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "GitHub_Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        Owner                = var.repo_owner
+        Repo                 = var.repo_name
+        Branch               = var.branch
+        OAuthToken           = var.github_token
+        PollForSourceChanges = "true"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+    action {
+      name             = "CodeBuild"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.build_project.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+    action {
+      name            = "CodeDeploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeploy"
+      version         = "1"
+      input_artifacts = ["build_output"]
+
+      configuration = {
+        ApplicationName     = aws_codedeploy_app.app.name
+        DeploymentGroupName = aws_codedeploy_deployment_group.deployment_group.deployment_group_name
+      }
+    }
+  }
+}
